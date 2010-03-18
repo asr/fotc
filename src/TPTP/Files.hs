8ec1681b250f92b2d475e7d9e21070ad7a9f2216
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2010-03-18 10:12:56 -0500

    Created the TPTP files associated with the EXTERNAL theorems.
    Ignore-this: 8555dfc7a09e8c97a325ab284bed0f09
    
    darcs-hash:20100318151256-3bd4e-81adfa607ee6933ac8880abd11deb5de10c88777.gz

--- src/TPTP/Files.hs
+++ src/TPTP/Files.hs
@@ -4,34 +4,76 @@
 
 module TPTP.Files where
 
+-- Haskell imports
+import System.FilePath
+
 -- Local imports
 import TPTP.Types
 
+------------------------------------------------------------------------------
+extTPTP :: String
+extTPTP = ".tptp"
+
 axiomsFile :: String
-axiomsFile = "/tmp/externalAxioms.tptp"
+axiomsFile = addExtension "/tmp/externalAxioms" extTPTP
+
+communHeader :: String
+communHeader =
+    "%-----------------------------------------------------------------------------\n" ++
+  "% This file was generated automatically.\n" ++
+  "%-----------------------------------------------------------------------------\n\n"
+
+communFooter :: String
+communFooter =
+  "\n" ++
+  "%-----------------------------------------------------------------------------\n"
 
 headerAxioms :: String
 headerAxioms =
-  "%-----------------------------------------------------------------------------\n" ++
-  "% This file was generated automatically.\n" ++
-  "%-----------------------------------------------------------------------------\n\n" ++
+  communHeader ++
   "% This file corresponds to the EXTERNAL axioms.\n\n"
 
 footerAxioms :: String
 footerAxioms =
-  "\n" ++
-  "%-----------------------------------------------------------------------------\n" ++
-  "% End EXTERNAL axioms.\n"
+    communFooter ++
+    "% End EXTERNAL axioms.\n"
 
-addLinesTPTP :: [LineTPTP] -> IO ()
-addLinesTPTP [] = return ()
-addLinesTPTP (line@(MkLineTPTP  _ AxiomTPTP _ ) : xs) =
-    appendFile axiomsFile (show line) >> addLinesTPTP xs
-addLinesTPTP (MkLineTPTP  _ ConjectureTPTP _  : xs) = addLinesTPTP xs
+headerTheorem :: String
+headerTheorem =
+    communHeader ++
+  "% This file corresponds to an EXTERNAL theorem.\n\n" ++
+  "% We include the EXTERNAL axioms file. \n" ++
+  "include('" ++ axiomsFile ++ "').\n\n"
 
+footerTheorem :: String
+footerTheorem =
+    communFooter ++
+    "% End EXTERNAL theorem.\n"
+
+addAxiom :: LineTPTP -> IO ()
+addAxiom line@(MkLineTPTP  _ AxiomTPTP _ ) = appendFile axiomsFile (show line)
+addAxiom _                                 = return ()
+
+createAxiomsFile :: [LineTPTP] -> IO ()
+createAxiomsFile linesTPTP = do
+  _ <- writeFile axiomsFile headerAxioms
+  _ <- mapM_ addAxiom linesTPTP
+  _ <- appendFile axiomsFile footerAxioms
+  return ()
+
+createTheoremFile :: LineTPTP -> IO ()
+createTheoremFile line@(MkLineTPTP  name ConjectureTPTP _ ) = do
+  let theoremFile = addExtension ("/tmp/" ++ name) extTPTP
+  _ <- writeFile theoremFile headerTheorem
+  _ <- appendFile theoremFile (show line)
+  _ <- appendFile theoremFile footerTheorem
+  return ()
+createTheoremFile _ = return ()
+
+-- We create a file with all the EXTERNAL axioms and we create a file
+-- for each EXTERNAL theorem.
 createFilesTPTP :: [LineTPTP] -> IO ()
 createFilesTPTP linesTPTP = do
-  _ <- writeFile axiomsFile headerAxioms
-  _ <- addLinesTPTP linesTPTP
-  _ <- appendFile axiomsFile footerAxioms
+  _ <- createAxiomsFile linesTPTP
+  _ <- mapM_ createTheoremFile linesTPTP
   return ()
