0d5129742b2c129f3bfbe6352f6128bf6b28e4d7
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2010-04-07 11:11:12 -0500

    Added validations.
    Ignore-this: 70c1566b6f31f65bcd0ee6b79cea8979
    
    darcs-hash:20100407161112-3bd4e-afa45912ebfcea8e9bd0129bada1e33f049aa405.gz

--- src/FOL/Primitives.hs
+++ src/FOL/Primitives.hs
@@ -1,25 +1,36 @@
 ------------------------------------------------------------------------------
 -- FOL names hard-coded in the translation from Agda types to FOL formulas
 ------------------------------------------------------------------------------
-
 -- Adapted from agdaLight (Plugins.FOL.Primitive).
+
+{-# LANGUAGE CPP #-}
 
 module FOL.Primitives where
 
+-- Agda library imports
+import Agda.Utils.Impossible ( Impossible(..), throwImpossible )
+
+-- Local imports
 import FOL.Types ( TermFOL(FunFOL), FormulaFOL(Predicate) )
+
+#include "../undefined.h"
+
+------------------------------------------------------------------------------
 
 -- This is just an arbitrary constant which we hope doesn't
 -- conflict with user constants.
-
-app :: [TermFOL] -> TermFOL
-app  = FunFOL kApp
-
 kApp :: String
 kApp = "kApp"
 
--- This will refer to the predefined equality in the ATP.
-equal :: [TermFOL] -> FormulaFOL
-equal   = Predicate kEqual
+app :: [TermFOL] -> TermFOL
+app (t1 : t2 : []) = FunFOL kApp [t1, t2]
+app _              = __IMPOSSIBLE__
 
+
+-- This will refer to the predefined equality in the ATP.
 kEqual :: String
 kEqual = "kEqual"
+
+equal :: [TermFOL] -> FormulaFOL
+equal (t1 : t2 : [])  = Predicate kEqual [t1, t2]
+equal _               = __IMPOSSIBLE__
