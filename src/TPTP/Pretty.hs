1150f502e16a0b7cb29daba18645ebdf4698d908
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2010-04-10 18:45:11 -0500

    Fixed the pretty-printer for minus (i.e. '-').
    Ignore-this: d6b8a09f8aae56ef7c507e2f0a120f98
    
    darcs-hash:20100410234511-3bd4e-7c730314936e7c426930c7f32fba303c95772735.gz

--- src/TPTP/Pretty.hs
+++ src/TPTP/Pretty.hs
@@ -51,7 +51,6 @@ changeToLower name = changeCaseFirstSymbol (prettyTPTP name) toLower
 
 instance PrettyTPTP Char where
     prettyTPTP c
-        | c == '-' = "_"
         | c == '.' = ""
         -- The character is a subscript number (i.e. ₀, ₁, ₂, ...).
         | ord c `elem` [8320 .. 8329] = [chr (ord c - 8272)]
