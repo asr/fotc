e989c1f73e899dcb1c8f645a06ecf2b654f0ea9b
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2010-07-16 10:46:59 -0500

    Removed unused code.
    Ignore-this: 1aa75b7835771bd5972afc7d916d5308
    
    darcs-hash:20100716154659-3bd4e-4dcf72c8ee1398ace63ad9d0319c267a2d53a3d2.gz

--- src/FOL/Constants.hs
+++ src/FOL/Constants.hs
@@ -4,8 +4,8 @@
 
 module FOL.Constants where
 
--- FOL constant names -----------------------------------------------------
-
+-----------------------------------------------------------------------------
+-- The FOL constant names.
 trueFOL, falseFOL, notFOL, andFOL, orFOL, existsFOL, equalsFOL :: String
 trueFOL   = "⊤"
 falseFOL  = "⊥"
@@ -16,18 +16,7 @@ existsFOL = "∃D"
 equalsFOL = "≡"
 
 -- These FOL constanst are not used because we use the Agda versions.
-
 -- impliesFOL, equivFOL , forAllFOL :: String
 -- impliesFOL = "Implies"
 -- equivFOL   = "Equiv"
 -- forAllFOL  = "ForAll"
-
-{-
-folConstants = [  fol_And, fol_Or, fol_Implies, fol_Equiv
-                , fol_Not, fol_Equals, fol_ForAll, fol_Exists
-                , fol_Prop, fol_True, fol_False
-                ]
-
-isFOLConstant :: String -> Bool
-isFOLConstant x = x `elem` folConstants
--}
