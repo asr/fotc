54765438e98cd751de85305a289bf99c88dd8651
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2010-07-21 14:12:33 -0500

    Only indentation.
    Ignore-this: 44842187ef0fbbacc619afd5def727b1
    
    darcs-hash:20100721191233-3bd4e-4eaecfdf1222733c23f2ff93fdc9dbf31ba06ca5.gz

--- src/FOL/Types.hs
+++ src/FOL/Types.hs
@@ -9,30 +9,31 @@ module FOL.Types where
 ------------------------------------------------------------------------------
 -- FOL formulas.
 -- Adapted from AgdaLight (Plugins.FOL.Types).
-data TermFOL = FunFOL String [TermFOL]
-             | VarFOL String
+data TermFOL = FunFOL   String [TermFOL]
+             | VarFOL   String
              | ConstFOL String -- AgdaLight hasn't them.
                deriving ( Show )
 
-data FormulaFOL = Predicate String [TermFOL]
-                | And FormulaFOL FormulaFOL
-                | Or FormulaFOL FormulaFOL
-                | Not FormulaFOL
-                | Implies FormulaFOL FormulaFOL
-                | Equiv FormulaFOL FormulaFOL
-                | ForAll String (TermFOL -> FormulaFOL)
-                | Exists String (TermFOL -> FormulaFOL)
-                | TRUE
+data FormulaFOL = TRUE
                 | FALSE
+                | Predicate String     [TermFOL]
+                | Not       FormulaFOL
+                | And       FormulaFOL FormulaFOL
+                | Or        FormulaFOL FormulaFOL
+                | Implies   FormulaFOL FormulaFOL
+                | Equiv     FormulaFOL FormulaFOL
+                | ForAll    String     (TermFOL -> FormulaFOL)
+                | Exists    String     (TermFOL -> FormulaFOL)
+
 
 instance Show FormulaFOL where
+    show TRUE                = " TRUE "
+    show FALSE               = " FALSE "
     show (Predicate name ts) = " Predicate " ++ show name ++ " " ++ show ts
+    show (Not f)             = " Not " ++ show f
     show (And f1 f2)         = " And " ++ show f1 ++ show f2
     show (Or f1 f2)          = " Or " ++ show f1 ++ show f2
-    show (Not f)             = " Not " ++ show f
     show (Implies f1 f2)     = " Implies " ++ show f1 ++ show f2
     show (Equiv f1 f2)       = " Equiv " ++ show f1 ++ show f2
     show (ForAll var f)      = " ForAll " ++ show var ++ show (f $ VarFOL var)
     show (Exists var f)      = " Exists " ++ show var ++ show (f $ VarFOL var)
-    show TRUE                = " TRUE "
-    show FALSE               = " FALSE "
