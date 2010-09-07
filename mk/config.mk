f625ce6db7e7997a74873873deb24d155a03c6fb
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2010-09-07 07:54:52 -0500

    Moved some stuff to mk/config.mk.
    Ignore-this: 9f392ae674e9e2ef6188ee5bbcb716d1
    
    darcs-hash:20100907125452-3bd4e-91a89f2e665ddde594ac14b20f15bf96df188932.gz

--- mk/config.mk
+++ mk/config.mk
@@ -1,2 +1,10 @@
 AGDA = agda -v 0
 RSYNC = rsync --archive --progress --rsh='ssh -p 2024'
+
+# Host directory used by publish
+# Tunneling connection
+root_host_dir = asicard@localhost:tmp
+
+clean :
+	-find -name '*.agdai' | xargs rm -f
+	-rm -f /tmp/*.tptp
