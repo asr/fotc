cebcbd0ca63829f552305f12dd68eb95469df5fe
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2011-04-05 14:35:42 -0500

    Induction-recursion is not allow in Coq.
    Ignore-this: 3d9872669e784c4a504f1403cd6bb50
    
    darcs-hash:20110405193542-3bd4e-8ed8acbfe0a18e89bf6b130aa8c4844761c785b6.gz

---
+++ Draft/FOTC/Program/Nest/Nest.v
@@ -0,0 +1,15 @@
+(* Nested recursive function using the Bove-Capretta method *)
+
+(* Induction-recursion is not allow in Coq 8.3 *)
+
+Require Import Coq.Unicode.Utf8.
+
+Inductive Dom : nat → Prop :=
+  | d0 : Dom 0
+  | dS : ∀ x, Dom x → Dom (nest x) → Dom (S x)
+with
+Fixpoint nest (n : nat)(d : Dom n) {struct d}: nat :=
+  match d with
+    | d0           => 0
+    | dS x Dx Dnx  => 0
+  end.
