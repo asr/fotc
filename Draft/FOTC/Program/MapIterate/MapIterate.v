b7dd13e59947ff55852747ffe60c8fed0fee5915
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2011-09-27 11:48:02 -0500

    Cosmetic changes.
    Ignore-this: 154275650de623f8fcab86338f08679d
    
    darcs-hash:20110927164802-3bd4e-80505e8405a4bfb3caa19036277806aa866217bc.gz

--- Draft/FOTC/Program/MapIterate/MapIterate.v
+++ Draft/FOTC/Program/MapIterate/MapIterate.v
@@ -3,7 +3,7 @@
 (* Tested with Coq 8.3pl2 *)
 
 (* From: Eduardo Giménez Pierre Castéran. A Tutorial on [Co-]Inductive
-Types in Coq May 1998 -- August 17, 2007 *)
+Types in Coq. May 1998 -- August 17, 2007. *)
 
 Require Import Unicode.Utf8.
 
@@ -20,9 +20,9 @@ CoFixpoint iterate {A : Type}(f : A → A)(a : A) : Stream A :=
   Cons A a (iterate f (f a)).
 
 CoFixpoint map {A B : Type}(f : A → B)(s : Stream A) : Stream B:=
-  match s with Cons a tl  => Cons B (f a) (map f tl) end.
+  match s with Cons a tl => Cons B (f a) (map f tl) end.
 
-CoInductive EqSt {A : Type} : Stream A → Stream A  → Prop :=
+CoInductive EqSt {A : Type} : Stream A → Stream A → Prop :=
   eqst : ∀ s1 s2 : Stream A,
          head s1 = head s2 →
          EqSt (tail s1) (tail s2) →
@@ -34,27 +34,24 @@ Variable R        : Stream A → Stream A → Prop.
 Hypothesis bisim1 : ∀ s1 s2 : Stream A, R s1 s2 → head s1 = head s2.
 Hypothesis bisim2 : ∀ s1 s2 : Stream A, R s1 s2 → R (tail s1) (tail s2).
 
-CoFixpoint park_ppl : ∀ s1 s2 : Stream A, R s1 s2 → EqSt s1 s2 :=
-  fun s1 s2 (p : R s1 s2) =>
-      eqst s1 s2
-           (bisim1 s1 s2 p)
-           (park_ppl (tail s1)
-           (tail s2)
-           (bisim2 s1 s2 p)).
+CoFixpoint park_ppl (s1 s2 : Stream A)(p : R s1 s2) : EqSt s1 s2 :=
+  eqst s1 s2
+       (bisim1 s1 s2 p)
+       (park_ppl (tail s1) (tail s2) (bisim2 s1 s2 p)).
+
 End Parks_Principle.
 
-Theorem map_iterate : forall {A : Type}(f : A → A)(x : A),
-                      EqSt (iterate f (f x)) (map f (iterate f x)).
+Theorem map_iterate {A : Type}(f : A → A)(x : A) :
+                    EqSt (iterate f (f x)) (map f (iterate f x)).
 Proof.
-intros A f x.
 apply park_ppl with
   (R := fun s1 s2 =>
         ∃ x : A, s1 = iterate f (f x) ∧
                  s2 = map f (iterate f x)).
-intros s1 s2 (x0,(eqs1,eqs2));
+intros s1 s2 (x0, (eqs1, eqs2));
   rewrite eqs1; rewrite eqs2; reflexivity.
-intros s1 s2 (x0,(eqs1,eqs2)).
-exists (f x0);split;
-  [rewrite eqs1|rewrite eqs2]; reflexivity.
-exists x;split; reflexivity.
+intros s1 s2 (x0, (eqs1, eqs2)).
+exists (f x0); split;
+  [rewrite eqs1 | rewrite eqs2]; reflexivity.
+exists x; split; reflexivity.
 Qed.
