ea4ee81669830e9e1d509d24087b60e3c17c9912
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2012-04-05 11:14:37 -0500

    Added bash script for converting FOF problems to Waldmeister problems.
    Ignore-this: 40d85504417a5818aa15ec9dfa8d30ce
    
    darcs-hash:20120405161437-3bd4e-600a16456416e922e415159dc07841a50d632cd2.gz

---
+++ utils/tptp2waldmeister.bash
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Tested with TPTP-v5.3.0 on 05 April 2012.
+
+# We make two transformations in the FOF problems:
+# 1. To transform FOF problems to CNF problems using Otter algorithm.
+# 2. To transform FOF problems to CNF problems in Waldmeister format
+#    using Otter algorithm.
+for file in *.tptp ; do
+    tptp2X -tcnf:otter $file
+    tptp2X -tcnf:otter -fwaldmeister $file
+done
