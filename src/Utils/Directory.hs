63c9025991b80cfac03c9fe0df67f3fa3e380bb4
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2012-07-03 07:33:46 -0500

    Minor change.

--- src/Utils/Directory.hs
+++ src/Utils/Directory.hs
@@ -25,8 +25,7 @@ import Data.Algorithm.Diff ( DI(F, S), getDiff )
 -- returns 'False'.
 diff ∷ FilePath → FilePath → IO Bool
 diff f1 f2 = do
-  l1 ← readFile f1
-  l2 ← readFile f2
+  [l1, l2] ← mapM readFile [f1, f2]
 
   let di ∷ [DI]
       di = fst $ unzip $ getDiff l1 l2
