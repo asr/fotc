527e1f834acf051fcc2a6d8bc8daa94d8d793ce2
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2012-09-13 22:44:49 -0500

    Removed warning.

--- notes/strictly-positive-inductive-types/Matthes.v
+++ notes/strictly-positive-inductive-types/Matthes.v
@@ -4,15 +4,15 @@ Inductive tree (A : Set) : Set :=
 |  nil_tree : tree A
 |  branch   : A → tree A.
 
-(* Error: Non strictly positive occurrence of "cont" in "((cont A → A) → *)
-(*  A) → cont A". *)
+(* Error: Non strictly positive occurrence of "cont" in *)
+(* "((cont A → A) →  A) → cont A". *)
 
 (* Inductive cont (A : Set) : Set := *)
 (* | nil_cont : cont A *)
 (* | c       : ((cont A → A) → A) → cont A. *)
 
-(* Error: Non strictly positive occurrence of "Tree" in "(tree Tree → *)
-(*  Tree) → Tree". *)
+(* Error: Non strictly positive occurrence of "Tree" in *)
+(* "(tree Tree → Tree) → Tree". *)
 
 (* Inductive Tree : Set := *)
 (* | nil_Tree  : Tree *)
