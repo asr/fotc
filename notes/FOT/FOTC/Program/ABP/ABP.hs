f72de1cc548e4fd92f700cb82edddef7fa227d73
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2013-02-14 11:49:20 -0500

    Using real streams in the ABP example.

--- notes/FOT/FOTC/Program/ABP/ABP.hs
+++ notes/FOT/FOTC/Program/ABP/ABP.hs
@@ -13,11 +13,11 @@
 
 module ABP where
 
+import Data.Stream.Infinite as S ( fromList, iterate, Stream((:>)), take )
+
 import System.Random ( newStdGen, randoms )
 
 ------------------------------------------------------------------------------
-
-type Stream a = [a]
 
 type Bit = Bool
 
@@ -27,34 +27,28 @@ data Err a  = Error | Ok a
 
 -- The mutual sender functions.
 send ∷ Bit → Stream a → Stream (Err Bit) → Stream (a, Bit)
-send _ []            _  = error "Impossible (abpsend)"
-send b input@(i : _) ds = (i , b) : await b input ds
+send b input@(i :> _) ds = (i , b) :> await b input ds
 
 await ∷ Bit → Stream a → Stream (Err Bit) → Stream (a, Bit)
-await _ _              []           = error "Impossible (await eq. 1)"
-await _ []             _            = error "Impossible (await eq. 2)"
-await b input@(i : is) (Ok b₀ : ds) =
-  if b == b₀ then send (not b) is ds else (i, b) : await b input ds
-await b input@(i : _) (Error : ds) = (i, b) : await b input ds
+await b input@(i :> is) (Ok b₀ :> ds) =
+  if b == b₀ then send (not b) is ds else (i, b) :> await b input ds
+await b input@(i :> _) (Error :> ds) = (i, b) :> await b input ds
 
 -- The receiver functions.
 ack ∷ Bit → Stream (Err (a, Bit)) → Stream Bit
-ack _ []                = error "Impossible (ack)"
-ack b (Ok (_, b₀) : bs) = if b == b₀
-                          then b : ack (not b) bs
-                          else not b : ack b bs
-ack b (Error : bs)      = not b : ack b bs
+ack b (Ok (_, b₀) :> bs) = if b == b₀
+                            then b :> ack (not b) bs
+                            else not b :> ack b bs
+ack b (Error :> bs)      = not b :> ack b bs
 
 out ∷ Bit → Stream (Err (a, Bit)) → Stream a
-out _ []                = error "Impossible (abpout)"
-out b (Ok (i, b₀) : bs) = if b == b₀ then i : out (not b) bs else out b bs
-out b (Error : bs)      = out b bs
+out b (Ok (i, b₀) :> bs) = if b == b₀ then i :> out (not b) bs else out b bs
+out b (Error :> bs)      = out b bs
 
 -- Model the fair unreliable transmission channel.
 corrupt ∷ Stream Bit → Stream a → Stream (Err a)
-corrupt (False : os) (_ : xs) = Error : corrupt os xs
-corrupt (True : os)  (x : xs) = Ok x  : corrupt os xs
-corrupt _            _        = error "Impossible (corrupt)"
+corrupt (False :> os) (_ :> xs) = Error :> corrupt os xs
+corrupt (True :> os)  (x :> xs) = Ok x  :> corrupt os xs
 
 -- The ABP transfer function.
 --
@@ -82,11 +76,11 @@ main = do
   gen₂ ← newStdGen
 
   let input ∷ Stream Int
-      input = [1 ..]
+      input = S.iterate (+ 1) 1
 
-      channel₁ , channel₂ ∷ [Bool]
-      channel₁ = randoms gen₁
-      channel₂ = randoms gen₂
+      channel₁ , channel₂ ∷ Stream Bool
+      channel₁ = S.fromList $ randoms gen₁
+      channel₂ = S.fromList $ randoms gen₂
 
       initialBit ∷ Bool
       initialBit = False
@@ -96,4 +90,4 @@ main = do
 
   print gen₁
   print gen₂
-  print (take 20 output)
+  print (S.take 20 output)
