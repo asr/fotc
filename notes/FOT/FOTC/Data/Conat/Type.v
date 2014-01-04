c9c4a51cd78da57ada98c8c1c675b0edee446aca
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2014-01-04 09:49:42 -0500

    Proved Conat_in.

--- notes/FOT/FOTC/Data/Conat/Type.v
+++ notes/FOT/FOTC/Data/Conat/Type.v
@@ -6,9 +6,9 @@ Axiom succ₁ : D → D.
 
 CoInductive Conat : D → Prop :=
   | cozero : Conat zero
-  | cosucc : ∀ n, Conat n → Conat (succ₁ n).
+  | cosucc : ∀ {n}, Conat n → Conat (succ₁ n).
 
-Theorem Conat_unf : ∀ n, Conat n → n = zero ∨ (∃ n', n = succ₁ n' ∧ Conat n').
+Theorem Conat_unf : ∀ {n}, Conat n → n = zero ∨ (∃ n', n = succ₁ n' ∧ Conat n').
 intros n Cn.
 case Cn.
 left.
@@ -17,4 +17,20 @@ intros n' Cn'.
 right.
 exists n'.
 auto.
+Qed.
+
+Theorem Conat_in : ∀ {n}, n = zero ∨ (∃ n', n = succ₁ n' ∧ Conat n') → Conat n.
+intros n h.
+case h.
+intro prf.
+subst.
+apply cozero.
+intros h'.
+elim h'.
+intros n'.
+intro prf.
+case prf.
+intros prf' Cn'.
+subst.
+apply (cosucc Cn').
 Qed.
