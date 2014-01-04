02bb348dabb1bf978c97a853a17841402b115fe9
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2014-01-04 09:04:53 -0500

    Removed a comment.

--- notes/co-data/Conat.v
+++ notes/co-data/Conat.v
@@ -4,11 +4,6 @@ CoInductive Conat : Set :=
   | cozero : Conat
   | cosucc : Conat → Conat.
 
-(* Reference Manual. Version 8.4pl3, §1.3.3: Notice that no principle of *)
-(* induction is derived from the definition of a co-inductive type, since *)
-(* such principles only make sense for inductive ones. For co-inductive *)
-(* ones, the only elimination principle is case analysis. *)
-
 CoFixpoint infty : Conat := cosucc infty.
 
 Fixpoint nat2conat (n : nat) : Conat :=
