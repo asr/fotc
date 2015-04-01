4980366d86f402146510a5fe9823e57782f75de0
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2015-04-01 15:14:48 -0500

    Added test for GHC.hs.

--- notes/FOT/FOTC/Program/GCD/GCD.hs
+++ notes/FOT/FOTC/Program/GCD/GCD.hs
@@ -1,9 +1,26 @@
+{-# LANGUAGE CPP           #-}
 {-# LANGUAGE UnicodeSyntax #-}
 
+#if __GLASGOW_HASKELL__ >= 710
+import Numeric.Natural
+#else
 import Data.Peano
+#endif
+
+import Test.QuickCheck
+
+#if __GLASGOW_HASKELL__ >= 710
+type Nat = Natural
+#endif
 
 gcd' ∷ Nat → Nat → Nat
 gcd' m n =
   if n == 0
   then m
   else if m == 0 then n else if m > n then gcd' (m - n) n else gcd' m (n - m)
+
+prop ∷ Nat → Nat → Bool
+prop m n = gcd' m n == gcd m n
+
+main ∷ IO ()
+main = quickCheck prop
