43e82b738e6e7f4492c228a75ba42b7fa1e879b9
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2016-07-15 12:16:11 -0500

    [ quicksort ] Removed unnecessary definition.

--- notes/FOT/FOTC/Program/QuickSort/DomainPredicate.v
+++ notes/FOT/FOTC/Program/QuickSort/DomainPredicate.v
@@ -1,16 +1,13 @@
 (* Domain predicate for quicksort using the Bove-Capretta method *)
 
-Require Import Arith.Compare_dec.
 Require Import Lists.List.
 Require Import Unicode.Utf8.
-
-Definition ltb (m n : nat) : bool := leb (S m) n.
 
 Inductive Dqs : list nat → Prop :=
   | dnil  : Dqs nil
   | dcons : ∀ x xs,
-            Dqs (filter (λ y, ltb y x) xs) →
-            Dqs (filter (λ y, ltb x y) xs) →
+            Dqs (filter (λ y, Nat.ltb y x) xs) →
+            Dqs (filter (λ y, Nat.ltb x y) xs) →
             Dqs (x :: xs).
 
 Check Dqs_ind.
